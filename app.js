fe1d4fa0ef50ec116830d9d27e86afab02754909
Roman <r.ledin.exceedteam@gmail.com>
2021-04-06 12:04:32 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const product = require('./routes/product.route'); // Imports routes for the products
+const app = express();
+app.use('/products', product);
+let port = 1235;
+app.listen(port, () => {
+    console.log('Server is up and running on port numner ' + port);
+});
