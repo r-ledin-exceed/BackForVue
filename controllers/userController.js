9d5ff6f1c75eba8da20b40dc5ce4b1455491ccd0
Roman <r.ledin.exceedteam@gmail.com>
2021-04-07 15:55:56 +0300

    Eslint fix

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,97 +1,89 @@
+/* eslint-disable no-underscore-dangle */
 const User = require('../models/model');
-const mongoose = require('mongoose');
 
 exports.addMarker = async (req, res) => {
-    const { coords, name } = req.body.items
-    const id = req.params.id
-    let currentUser = await User.findById(id)
+  const { coords, name } = req.body.items;
+  const { id } = req.params;
+  const currentUser = await User.findById(id);
+  const check = Array.isArray(coords);
 
-    if (!(coords) || !(name)) {
-        return res.status(400).send({
-            response: "error",
-            message: "fill all inputs"
-        })
-    }
+  if (!(coords) || !(name)) {
+    return res.status(400).send({
+      response: 'error',
+      message: 'fill all inputs',
+    });
+  }
 
-    if ((typeof(coords) != Array) && (coords.length != 2))
-    {
-        return res.status(400).send({
-            response: "error",
-            message: "enter correct coords"
-        })
-    }
+  if (!check || (coords.length !== 2)) {
+    return res.status(400).send({
+      response: 'error',
+      message: 'enter correct coords',
+    });
+  }
 
-    currentUser.items.push({coords, name});
-    console.log(currentUser)
-    await currentUser.save();
+  currentUser.items.push({ coords, name });
+  await currentUser.save();
 
-    return res.status(200).send({
-        response: "ok",
-        items: {coords, name}
-    })
-
+  return res.status(200).send({
+    response: 'ok',
+    items: { coords, name },
+  });
 };
 
 exports.updMarker = async (req, res) => {
-    const { coords, name } = req.body.items
-    const { marker, id } = req.params
-    const currentUser = await User.findById(id)
+  const { coords, name } = req.body.items;
+  const { marker, id } = req.params;
+  const currentUser = await User.findById(id);
+  const check = Array.isArray(coords);
+  const indexMarker = currentUser.items
+    .findIndex((item) => item._id.toString() === marker);
 
-    if (!(coords) || !(name)) {
-        return res.status(400).send({
-            response: "error",
-            message: "fill all inputs"
-        })
-    }
-
-    if ((typeof(coords) != Array) && (coords.length != 2))
-    {
-        return res.status(400).send({
-            response: "error",
-            message: "enter correct coords"
-        })
-    }
-
-    currentUser.items.forEach(item => {
-        if (marker == item._id) {
-            item.coords = coords
-            item.name = name
-        }
+  if (!(coords) || !(name)) {
+    return res.status(400).send({
+      response: 'error',
+      message: 'fill all inputs',
     });
+  }
 
-    await currentUser.save()
+  if (!check && (coords.length !== 2)) {
+    return res.status(400).send({
+      response: 'error',
+      message: 'enter correct coords',
+    });
+  }
 
-    return res.status(200).send({
-        response: "ok",
-        items: {coords, name}
-    })
-}
+  currentUser.items[indexMarker].coords = coords;
+  currentUser.items[indexMarker].name = name;
+
+  await currentUser.save();
+
+  return res.status(200).send({
+    response: 'ok',
+    items: { coords, name },
+  });
+};
 
 exports.removeMarker = async (req, res) => {
-    const { marker, id } = req.params
-    const currentUser = await User.findById(id)
+  const { marker, id } = req.params;
+  const currentUser = await User.findById(id);
+  const indexMarker = currentUser.items
+    .findIndex((item) => item._id.toString() === marker);
 
-    currentUser.items.forEach((item, index) => {
-        if (marker == item._id) {
-            currentUser.items.splice(index, 1)
-        }
-    });
+  currentUser.items.splice(indexMarker, 1);
 
-    await currentUser.save()
+  await currentUser.save();
 
-    return res.status(200).send({
-        response: "deleted succesfully",
-    })
-    
-}
+  return res.status(200).send({
+    response: 'deleted succesfully',
+  });
+};
 
 exports.allMarkers = async (req, res) => {
-    const id = req.params.id
-    const currentUser = await User.findById(id)
+  const { id } = req.params;
+  const currentUser = await User.findById(id);
 
-    return res.status(200).send({
-        response: "list of markers",
-        list: currentUser.items,
-    })
-
-}
+  return res.status(200).send({
+    response: 'list of markers',
+    list: currentUser.items,
+  });
+};
--- models/model.js
+++ models/model.js
@@ -1,18 +1,19 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
-let User = new Schema({
+const { Schema } = mongoose;
 
-    email: {type: String},
-    password: {type: String},
+const User = new Schema({
 
-    items: [{
-        name: {type: String},
-        coords: [Number, Number],
-        url: {type: String},
-    }]
-    
-    },
+  email: { type: String },
+  password: { type: String },
+
+  items: [{
+    name: { type: String },
+    coords: [Number, Number],
+    url: { type: String },
+  }],
+
+},
 { versionKey: false });
 
 // Export the model
